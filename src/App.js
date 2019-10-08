de4e0998f03c66485fffcf025d2c9b557c98bd77
chrishaig14 <chrishaig14@gmail.com>
2019-10-07 23:28:42 -0300

    Add div to show coords on click

--- src/App.js
+++ src/App.js
@@ -9,18 +9,24 @@ class App extends React.Component {
 // first, we use Dojo's loader to require the map class
         const options = {css: true};
 
-        loadModules(["esri/views/MapView", "esri/WebMap"], options)
-            .then(([MapView, WebMap]) => {
-                // then we load a web map from an id
-                var webmap = new WebMap({
-                    portalItem: { // autocasts as new PortalItem()
-                        id: "f2e9b762544945f390ca4ac3671cfa72"
-                    }
+        loadModules(["esri/Map", "esri/views/MapView"], options)
+            .then(([Map, MapView]) => {
+                console.log("MAP: ", Map);
+                var map = new Map({
+                    basemap: "streets"
                 });
-                // and we show that map in a container w/ id #viewDiv
+
                 var view = new MapView({
-                    map: webmap,
-                    container: "viewDiv"
+                    container: "viewDiv",
+                    map: map,
+                    center: [-118.71511, 34.09042], // longitude, latitude
+                    zoom: 11
+                });
+                view.on("click", (e) => {
+                    // console.log("e.x: ", e.x, " e.y: ", e.y);
+                    let pos = view.toMap({x: e.x, y: e.y});
+                    pos = {latitude: pos.latitude, longitude: pos.longitude};
+                    document.getElementById("coordinates").innerText = pos.latitude + " , " + pos.longitude;
                 });
             })
             .catch(err => {
@@ -33,6 +39,7 @@ class App extends React.Component {
         return (
             <div className="App">
                 <div id={"viewDiv"} style={{width: "400px", height: "400px"}}></div>
+                <div id={"coordinates"}></div>
             </div>
         );
     }
