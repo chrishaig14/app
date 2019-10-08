fc2a10449ae291302b03fda2666b16f9c4c77271
chrishaig14 <chrishaig14@gmail.com>
2019-10-08 13:15:45 -0300

    Add feature to layer on click

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ class App extends React.Component {
 // first, we use Dojo's loader to require the map class
         const options = {css: true};
 
-        loadModules(["esri/Map", "esri/views/MapView"], options)
-            .then(([Map, MapView]) => {
+        loadModules(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/Graphic"], options)
+            .then(([Map, MapView, FeatureLayer, Graphic]) => {
                 console.log("MAP: ", Map);
+
                 var map = new Map({
                     basemap: "streets"
                 });
@@ -22,11 +23,41 @@ class App extends React.Component {
                     center: [-118.71511, 34.09042], // longitude, latitude
                     zoom: 11
                 });
+
+                var features = [{geometry: {type: "point", x: -20, y: 80}, attributes: {ObjectID: 1, name: "HELLO"}}];
+                var graphics = features.map(feature => Graphic.fromJSON(feature));
+                var featureLayer = new FeatureLayer({
+                    id: "points",
+                    fields: [
+                        {
+                            name: "ObjectID",
+                            alias: "ObjectID",
+                            type: "oid"
+                        }, {
+                            name: "type",
+                            alias: "Type",
+                            type: "string"
+                        }],
+                    objectIdField: "ObjectID",
+                    geometryType: "point",
+                    source: graphics
+                });
+                map.layers.add(featureLayer);
                 view.on("click", (e) => {
                     // console.log("e.x: ", e.x, " e.y: ", e.y);
                     let pos = view.toMap({x: e.x, y: e.y});
                     pos = {latitude: pos.latitude, longitude: pos.longitude};
                     document.getElementById("coordinates").innerText = pos.latitude + " , " + pos.longitude;
+                    var newFeature = {
+                        geometry: {type: "point", x: pos.longitude, y: pos.latitude},
+                        attributes: {ObjectID: 2, name: "HELLO"}
+                    };
+                    featureLayer.applyEdits({addFeatures: [Graphic.fromJSON(newFeature)]})
+                        .then(res => {
+                            let l = map.findLayerById("points");
+                            console.log(l);
+                        })
+                        .catch(err => console.log("ERROR: ", err));
                 });
             })
             .catch(err => {
