1a27707ae885b78168f4d323be75cc677197d689
chrishaig14 <chrishaig14@gmail.com>
2019-10-07 21:24:04 -0300

    Initial commit: show map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {loadModules} from "esri-loader";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    componentDidMount() {
+
+// first, we use Dojo's loader to require the map class
+        const options = {css: true};
+
+        loadModules(["esri/views/MapView", "esri/WebMap"], options)
+            .then(([MapView, WebMap]) => {
+                // then we load a web map from an id
+                var webmap = new WebMap({
+                    portalItem: { // autocasts as new PortalItem()
+                        id: "f2e9b762544945f390ca4ac3671cfa72"
+                    }
+                });
+                // and we show that map in a container w/ id #viewDiv
+                var view = new MapView({
+                    map: webmap,
+                    container: "viewDiv"
+                });
+            })
+            .catch(err => {
+                // handle any errors
+                console.error(err);
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div id={"viewDiv"} style={{width: "400px", height: "400px"}}></div>
+            </div>
+        );
+    }
 }
 
 export default App;
